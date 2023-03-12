cbfd11186f9048ba8b0c931f4adc97154d42121f
Arctanyn <m.dugulubgov@icloud.com>
2023-03-12 14:00:55 +0300

    Initial Commit

---
+++ Text-Encoder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Text-Encoder
+//
+//  Created by Малиль Дугулюбгов on 12.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text-Encoder/Text_EncoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_EncoderApp.swift
+//  Text-Encoder
+//
+//  Created by Малиль Дугулюбгов on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Text_EncoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
